a74df57ee4bc1e6040b8237ab74cb4a629c1363d
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-10-24 17:25:23 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatefulWidget {
+  @override
+  _FlashChatState createState() => _FlashChatState();
+}
+
+class _FlashChatState extends State<FlashChat> {
+  late Future<FirebaseApp> _initialization;
+
+  @override
+  void initState() {
+    _initialization = Firebase.initializeApp();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print(snapshot.error);
+          return const Center(
+            child: Text('Unable to initialize database'),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            initialRoute: WelcomeScreen.id,
+            routes: {
+              WelcomeScreen.id: (context) => WelcomeScreen(),
+              LoginScreen.id: (context) => LoginScreen(),
+              RegistrationScreen.id: (context) => RegistrationScreen(),
+              ChatScreen.id: (context) => ChatScreen(),
+            },
+          );
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
